bfc878f929b55ed90c53f202c6f3b23947c4fe01
LiranS <lirangamerz@gmail.com>
2023-01-29 16:45:01 +0200

    categories localStorage based

--- src/store/Categories.js
+++ src/store/Categories.js
@@ -1,12 +1,18 @@
 import { create } from "zustand";
+
+const updateLocalStorageCategories = (categories) => localStorage.setItem('categories',JSON.stringify(categories));
+const initialCategories = JSON.parse(localStorage.getItem('categories')) ?? ['food','salary','bills','stocks','clothing','groceries','rent','car','medical'];
+
+
 const useCategoriesStore = create((set)=>({
-    categories:['food','salary','bills','stocks','clothing','groceries','rent','car','medical'],
+    categories:initialCategories,
 
     addCategory:(category) => set((state)=>{
         const cate = category.toLowerCase();
         if (state.categories.includes(cate)) return state.categories;
         const fornow = [...state.categories];
         fornow.push(cate);
+        updateLocalStorageCategories(fornow);
         return {categories:fornow};
        
     }),
@@ -15,6 +21,7 @@ const useCategoriesStore = create((set)=>({
         const current = [...state.categories];
         const index = current.indexOf(category);
         current.splice(index,1);
+        updateLocalStorageCategories(current);
         return {categories:current};
     })
 }))
